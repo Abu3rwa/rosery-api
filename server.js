fe7f4a09464f51fbe387d3e8c0730455373ecad9
Abu3rwa <Abu3rwa.abdulhafeez@gmail.com>
2023-10-03 14:10:08 +0200

    INIT

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ db();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["https://rosery-school.onrender.com", "http://localhost:3000"],
     methods: "GET, POST, PUT, DELETE, PATCH",
     credentials: true,
   })
